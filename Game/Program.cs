fa0e83e166873a3d58fcdf280ea3c5d436bcf1d7
PvttJebus <149273873+PvttJebus@users.noreply.github.com>
2024-03-19 11:17:58 -0400

    -Becoming a Star

--- Game/Program.cs
+++ Game/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Somebody once told me");
+            Console.WriteLine("The world is gonna roll me");
+            Console.WriteLine("I ain't the sharpest tool in the shed");
+            Console.WriteLine("She was looking kind of dumb");
+            Console.WriteLine("With her finger and her thumb");
+            Console.WriteLine("In the shape of an \"L\" on her forehead");
+            Console.WriteLine("Well, the years start coming\r\nAnd they don't stop coming\r\nFed to the rules and I hit the ground running\r\nDidn't make sense not to live for fun\r\nYour brain gets smart, but your head gets dumb");
+            Console.WriteLine("So much to do, so much to see\r\nSo what's wrong with taking the back streets?\r\nYou'll never know if you don't go\r\nYou'll never shine if you don't glow\r\nHey now, you're an all star\r\nGet your game on, go play\r\nHey now, you're a rock star\r\nGet the show on, get paid\r\nAnd all that glitters is gold\r\nOnly shooting stars break the mold");
+
+
+
         }
     }
 }
